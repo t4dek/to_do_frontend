f793d40e75daf624be86d0d7c3a1f55102ed3a39
Ted <t4dek01@gmail.com>
2015-09-02 17:17:13 +0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toDoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the toDoAppApp
+ */
+angular.module('toDoAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
