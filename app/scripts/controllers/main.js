cc511c652dc340a4066faabdb9f9e1d8d77fe207
Ted <t4dek01@gmail.com>
2015-09-21 23:34:49 +0300

    Implemented new tasks adding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,10 +5,50 @@ angular.module('toDoApp')
     
     $scope.new = false;
     $scope.showEdit = false;
-    $scope.editable = false;
     $scope.errors = [];
     $scope.lists = [];
     $scope.tasks = [];
+    $scope.currentList;
+    
+    retrieveLists();
+    
+    $scope.showTasks = function(listId){
+      $scope.currentList = listId;
+      retrieveTasks(listId);
+    };
+    
+    $scope.addList = function(params){
+      if (params.title){
+        ToDo.createList(params).then(function() {
+          retrieveLists();
+        })
+      } else {
+          $scope.errors.push("Enter a name of a list first!");
+      }
+    };
+    
+    $scope.addTask = function(params){
+      if (params.title){
+        ToDo.createTask(params.list_id, params).then(function(){
+          retrieveTasks(params.list_id);
+        })
+      } else {
+        $scope.errors.push("Enter a name of a task first!")
+      }
+    }
+    
+    $scope.editList = function(id, title){
+      if (title){
+        ToDo.editList(id, {title: title})
+      } else {
+        $scope.errors.push("Your list should have a name!");
+      }
+    };
+    
+    $scope.makeEditable = function(list){
+      list.editable = true;
+      
+    }
     
     function retrieveLists(){
       ToDo.allLists().then(function(response){
@@ -16,33 +56,9 @@ angular.module('toDoApp')
       });
     }
     
-    retrieveLists();
-    
-    $scope.showTasks = function(listId){
+    function retrieveTasks(listId){
       ToDo.listTasks(listId).then(function(response){
         $scope.tasks = response.data;
       });
-    };
-    
-    $scope.addList = function(title){
-      if (title){
-        ToDo.createList(title);
-        retrieveLists();
-        console.log($scope.lists);
-      } else {
-          $scope.errors.push("Enter a name of a list first!");
-      }
-    };
-    
-    $scope.editList = function(title){
-      if (title){
-        ToDo.editList(title)
-      } else {
-        $scope.errors.push("Your list should have a name!");
-      }
-    };
-    
-    $scope.makeEditable = function(){
-      $scope.editable = true;
     }
   }]);
--- app/scripts/services/todo.js
+++ app/scripts/services/todo.js
@@ -4,18 +4,20 @@ angular.module('toDoApp')
   .factory('ToDo',['$http', function($http){
     return {
       allLists: function(){
-        console.log("HERE");
         return $http.get("http://localhost:3000/lists");
       },
       listTasks: function(listId){
         return $http.get("http://localhost:3000/lists/" + listId + "/tasks");
       },
-      createList: function(title){
-        console.log(title);
-        return $http.post("http://localhost:3000/lists", {title});
+      createList: function(params){
+        return $http.post("http://localhost:3000/lists", params);
       },
       editList: function(listId, params){
         return $http.put("http://localhost:3000/lists/" + listId, params)
+      },
+      createTask: function(listId, params){
+        console.log(params);
+        return $http.post("http://localhost:3000/lists/" + listId + "/tasks", params)
       }
     };
   }]);
